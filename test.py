8819aca237de6f6d57f68f7ae89d560b08faea58
Brian Wu <fslikeyou1033@yahoo.com>
2021-02-23 16:50:42 +0800

    Sample Display

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 import cv2
 import numpy as np
+import utlis
 
 path = '1.jpg'
+
+img = cv2.imread(path)
+
+img = cv2.resize(img, (500, 700))
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+imgCanny = cv2.Canny(imgBlur, 100, 110)
+
+imageArray = ([img, imgGray, imgBlur, imgCanny])
+
+imgStack = utlis.stackImages(imageArray, 0.5)
+
+cv2.imshow('Sample Display', imgStack)
+cv2.waitKey(0)
