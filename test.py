35ab1d6df0a12dec6c7b4fca23213327302a336b
Brian Wu <fslikeyou1033@yahoo.com>
2021-04-26 17:20:27 +0800

    test

--- test.py
+++ test.py
@@ -15,8 +15,8 @@ imgCanny = cv2.Canny(imgBlur, 100, 110)
 cimg = cv2.cvtColor(imgBlur, cv2.COLOR_GRAY2BGR)
 
 # Find all contours
-#contours, hierachy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-#cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 5)
+contours, hierachy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 5)
 
 circles = cv2.HoughCircles(imgBlur, cv2.HOUGH_GRADIENT, 1, 20, param1 = 500, param2 = 40, minRadius = 0, maxRadius = 0)
 circles = np.uint16(np.around(circles))
@@ -25,10 +25,13 @@ for i in circles[0, :]:
 
 
 
-#imgBlank = np.zeros_like(img)
-#imageArray = ([img, imgGray, imgBlur, imgCanny],
-#              [imgContours, imgBlank, imgBlank, imgBlank])
-#imgStack = utlis.stackImages(imageArray, 0.5)
+imgBlank = np.zeros_like(img)
+imageArray = ([img, imgGray, imgBlur, imgCanny],
+              [imgContours, imgBlank, imgBlank, imgBlank])
+imgStack = utlis.stackImages(imageArray, 0.5)
 
+cv2.imshow('dd', imgStack)
 cv2.imshow('Sample Display', cimg)
 cv2.waitKey(0)
+
+# plaque counting
